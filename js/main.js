ccc3af3780232a4b8a190ce505f43e25edbec214
Anastasia Ganyushkina <kaise95@gmail.com>
2023-04-14 14:39:26 +0400

    feat: add light theme

--- js/main.js
+++ js/main.js
@@ -51,3 +51,35 @@ prevButton.addEventListener('click', () => {
         sliderItems[currentProject].classList.remove('hidden');
     }
 })
+
+// Switch theme 
+
+var btnSwitchTheme = document.querySelector('.icon-switch-theme');
+var btnToLight = document.querySelector('.sun-to-light');
+var btnToDark = document.querySelector('.sun-to-dark');
+var body = document.querySelector('body');
+var themeColor = document.querySelectorAll('.theme');
+var footer = document.querySelector('footer');
+
+btnSwitchTheme.addEventListener('click', () => {
+    if (btnToLight.classList.contains('active')) {
+        btnToLight.classList.remove('active');
+        btnToDark.classList.add('active');
+        body.style.background = '#FFFCF5';
+        body.style.transition = '1s';
+        footer.style.background = 'rgb(211, 144, 42, 0.2)';
+        themeColor.forEach(item => {
+            item.style.color = '#0C0B05';
+        })
+    }
+    else if (btnToDark.classList.contains('active')) {
+        btnToLight.classList.add('active');
+        btnToDark.classList.remove('active');
+        body.style.background = '#0C0B05';
+        body.style.transition = '1s';
+        footer.style.background = 'rgb(255, 252, 245, 0.2)';
+        themeColor.forEach(item => {
+            item.style.color = '#FFFCF5';
+        })
+    }
+})
